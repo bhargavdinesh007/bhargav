2573b70075b366b2b0e83de99b0f8037a9e8e123
bhargavdinesh007 <48099470+bhargavdinesh007@users.noreply.github.com>
2019-03-15 00:33:38 +0530

    Add files via upload

---
+++ gemini1/src/org/jsp/app/PostHotel.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jsp.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Oops
+ */
+public class PostHotel extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+            String s1 = request.getParameter("hname");
+           String s2 = request.getParameter("city");
+           
+           String s3 = request.getParameter("address");
+           String s4 = request.getParameter("car");
+           String s5 = request.getParameter("description");
+           String s6 = request.getParameter("contact");
+        try {
+            /* TODO output your page here. You may use following sample code. */
+          Class.forName("com.mysql.jdbc.Driver");
+      //  out.println("driver loaded");
+     Connection  con = DriverManager.getConnection("jdbc:mysql://localhost:3306/travel","root" ,"root");
+      //  out.println("Connect");
+      Statement  st =  con.createStatement();
+      st.executeUpdate("insert into travel.hotel (hotel_name,hotel_city,hotel_address,car,description,contact_no) values('"+s1+"','"+s2+"','"+s3+"','"+s4+"','"+s5+"','"+s6+"') ");
+      response.sendRedirect("hotel.jsp");
+      
+      
+      
+      
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(PostHotel.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(PostHotel.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
